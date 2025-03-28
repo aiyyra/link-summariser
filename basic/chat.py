36094d61090b23c9211c52ad3c00000ce6bc454e
Afiq <muhdafiq197@gmail.com>
2025-03-28 09:48:52 +0800

    adding basic chat programme and updating readme for progress log

---
+++ basic/chat.py
@@ -0,0 +1,53 @@
+from dotenv import load_dotenv
+
+from langchain.schema import AIMessage, HumanMessage, SystemMessage # Gemini would not have system message, but we will keep the import in case for model changes
+from langchain_google_genai import ChatGoogleGenerativeAI 
+
+
+# load api key
+load_dotenv()
+
+# Init chat model
+model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+# Init chat history
+chat_history = [] # a list to store history of chat
+
+# SystemMessage is use for the system wide instruct, however we cannot use it for Gemini model.
+# HumanMessage is to encapsulate the message sent by user into the messages list.
+# messages = [
+#     # SystemMessage(content="Solve the following math problems.") # We will keep here for model changes
+#     HumanMessage(content="What is 81 divided by 9?")
+# ]
+
+# # We can keep this here to make a system message when suitable model is used
+# system_message = SystemMessage(content="You are a helpful AI assistant.")
+# chat_history.append(system_message)  # Add system message to chat history
+
+# Chat loop
+while True:
+    query = input("You: ")
+    if query.lower() == "exit":
+        break
+    # Append the message into chat history
+    chat_history.append(HumanMessage(content=query))
+
+    # Get AI response based on the chat history
+    result = model.invoke(chat_history)
+    # Append to chat history and display the response
+    response = result.content
+    chat_history.append(AIMessage(content=response))
+    print(f"AI: {response}")
+
+print("---- Message History ----")
+print(chat_history)
+
+# result = model.invoke(messages)
+# print(f"Answer from AI: {result.content}")
+
+# # Invoke the model with a message
+# result = model.invoke("What is 81 divided by 9?")
+# print("Full result:")
+# print(result)
+# print("Content only:")
+# print(result.content)
